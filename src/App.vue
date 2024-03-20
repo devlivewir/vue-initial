81eb7296e2b317ec99dc42d92e08943bcfcce786
Ahmad Malik <85894787+ciahmad@users.noreply.github.com>
2024-03-21 01:54:02 +0500

    form handling

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
   <div>
     <template>
-      {{ 2 + 3 + 6 }}
-      <h2>hheha</h2>
-      <h6>Add method = {{ add(10, 20, 30) }}</h6>
-      <h6>Add method = {{ add(40, 50, 60) }}</h6>
-      <h6>Add method = {{ add(70, 80, 90) }}</h6>
-      <h6>Add * {{ mult(300) }}</h6>
       <div>
-        <button v-on:click="name = 'Bat Man'">Change the Name</button>
+        <pre>
+          {{ JSON.stringify(formValues, null, 2) }}
+        </pre>
       </div>
-      <h2>{{ count }}</h2>
+      <form action="">
+        <div>
+          <label for="name">Name</label>
+          <input type="text" id="name" v-model="formValues.name">
+        </div>
 
-      <button v-on:click="count +=  1">Increment</button>
-      <button v-on:click="count -=  1">Decrement</button>
+        <div>
+          <label for="summary">Summary</label>
+          <textarea id="summary" v-model="formValues.summary"/>
+        </div>
+
+        <div>
+          <label for="country">Country</label>
+          <select name="" id="country" v-model="formValues.country">
+            <option value="">Select Country</option>
+            <option value="Pakistan">Pakistan</option>
+            <option value="India">India</option>
+          </select>
+        </div>
+
+        <div>
+          <label for="job-location">Job Location</label>
+          <select name="" id="job-location" multiple v-model="formValues.jobLocation">
+            <option value="">Select Country</option>
+            <option value="Pakistan">Pakistan</option>
+            <option value="India">India</option>
+          </select>
+        </div>
+      </form>
     </template>
   </div>
 </template>
@@ -24,27 +45,30 @@ export default {
   name: 'App',
   data() {
     return {
-      baseMultiplyer: 5,
-      name: "alic",
-      count: 0,
-
+      formValues: {
+        name: '',
+        'summary':'',
+        'country':'',
+        'jobLocation':[]
+      }
     }
-  },
-  methods: {
-    shuffle() {
-      // console.log(this.names);
-      //this.names = _.shuffle(this.names);
-    },
-    add(a, b, c) {
-      return a + b + c
-    },
-    mult(num) {
-      return num * this.baseMultiplyer
-    },
-  },
-
+  }
 }
 </script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.underline {
+  text-decoration: underline;
+}
+</style>
 
 <style>
 #app {
