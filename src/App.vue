443813baea73d2af06c433dda76239c2fcf5940f
Ahmad Malik <85894787+ciahmad@users.noreply.github.com>
2024-03-26 01:36:04 +0500

    property vs method

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@
       <h2>Computed FullName - {{ FullName }}</h2>
       <h2>Total -{{ items.reduce((total, curr) => (total = total + curr.price), 0) }} </h2>
       <h2>with total computed method - {{ total }} </h2>
+      <button @click="items.push({ id: 4, title: 'keyBoard', price: 50 })">Add item</button>
+      <h2>Get Total  Method -{{ getTotal() }}</h2>
+      <input type="text" v-model="country">
     </template>
   </div>
 
@@ -34,16 +37,22 @@ export default {
           title: 'Screen',
           price: 170
         },
-      ]
+      ],
+      country:''
     }
   },
   methods: {
+    getTotal() {
+      console.log('get Total Method');
+      return this.items.reduce((total, curr) => (total = total + curr.price), 0)
+    }
   },
   computed: {
     FullName() {
       return `${this.firstName} ${this.lastName}`
     },
     total() {
+      console.log('get total property');
       return this.items.reduce((total, curr) => (total = total + curr.price), 0)
     }
 
