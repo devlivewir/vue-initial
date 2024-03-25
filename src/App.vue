c5fb86cee52f90af2e17f669e76b39572177246c
Ahmad Malik <85894787+ciahmad@users.noreply.github.com>
2024-03-26 01:55:45 +0500

    property vs method

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
-    <template>
-      <h2>FullName - {{ firstName }} {{ lastName }}</h2>
-      <h2>Computed FullName - {{ FullName }}</h2>
-      <h2>Total -{{ items.reduce((total, curr) => (total = total + curr.price), 0) }} </h2>
-      <h2>with total computed method - {{ total }} </h2>
-      <button @click="items.push({ id: 4, title: 'keyBoard', price: 50 })">Add item</button>
-      <h2>Get Total  Method -{{ getTotal() }}</h2>
-      <input type="text" v-model="country">
-    </template>
-  </div>
+    <h2>FullName - {{ firstName }} {{ lastName }}</h2>
+    <h2>Computed FullName - {{ FullName }}</h2>
+    <h2>Total - {{ getTotal() }}</h2>
+    <h2>with total computed method - {{ total }}</h2>
+    <button @click="addItem">Add item</button>
+    <h2>Get Total Method - {{ getTotal() }}</h2>
+    <input type="text" v-model="country">
+    <div v-for="item in items" :key="item.id">
+      <h2>{{ item.title }} - {{ item.price }}</h2>
+    </div>
 
+    <h2 v-for="item in expensiveItems" :key="item.id">
+      {{ item.title }}
+      -
+      {{ item.price }}
+    </h2>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   data() {
@@ -38,41 +43,30 @@ export default {
           price: 170
         },
       ],
-      country:''
+      country: ''
     }
   },
   methods: {
+    addItem() {
+      this.items.push({ id: 4, title: 'keyBoard', price: 50 });
+    },
     getTotal() {
-      console.log('get Total Method');
-      return this.items.reduce((total, curr) => (total = total + curr.price), 0)
+      return this.items.reduce((total, curr) => total + curr.price, 0);
     }
   },
   computed: {
     FullName() {
-      return `${this.firstName} ${this.lastName}`
+      return `${this.firstName} ${this.lastName}`;
     },
     total() {
-      console.log('get total property');
-      return this.items.reduce((total, curr) => (total = total + curr.price), 0)
+      return this.getTotal();
+    },
+    expensiveItems() {
+      return this.items.filter(item => item.price > 40);
     }
-
   }
 }
 </script>
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-.underline {
-  text-decoration: underline;
-}
-</style>
 
 <style>
 #app {
