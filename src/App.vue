abc9247d9d018e46976e8c067cca12adaf5b2bc6
Ahmad Malik <85894787+ciahmad@users.noreply.github.com>
2024-03-27 03:23:58 +0500

    props validation and type

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,20 @@
     <Greet name="alli" heroName="dupper" />
     <Greet name="ahi" heroName="happer" />
     <Greet :name="name" :heroName="channel" />
+    
+    <Artical  :likes="50" :isBoolean="true"/>
   </div>
 
 </template>
 
 <script>
 import Greet from './components/Greets.vue'
+import Artical from './components/Artical.vue'
 export default {
   name: 'App',
   components: {
     Greet,
+    Artical,
   },
   data() {
     return {
---
+++ src/components/Artical.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Article Component</h2>
+        <h2>{{ title }}</h2>
+        <h2>{{ likes }}</h2>
+        <h2>is published - {{ isBoolean ? 'Yes' : 'No' }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Artical',
+    props: {
+        title: {
+            type: String,
+            required: true,
+            default: 'Default title String'
+        },
+        likes: Number,
+        isBoolean: Boolean
+    }
+}
+</script>
+
+<style scoped></style>
