c0c51949977ce20b0135b23a4ce7eb601bb942cd
Ahmad Malik <85894787+ciahmad@users.noreply.github.com>
2024-03-27 01:47:28 +0500

    watcher

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
 <template>
   <div>
-    <h2>FullName - {{ firstName }} {{ lastName }}</h2>
-    <h2>Computed FullName - {{ FullName }}</h2>
-    <h2>Total - {{ getTotal() }}</h2>
-    <h2>with total computed method - {{ total }}</h2>
-    <button @click="addItem">Add item</button>
-    <h2>Get Total Method - {{ getTotal() }}</h2>
-    <input type="text" v-model="country">
-    <div v-for="item in items" :key="item.id">
-      <h2>{{ item.title }} - {{ item.price }}</h2>
-    </div>
+    <h2>Volum tracker (0 20)</h2>
+    <h3>Current Volume => {{ volume }}</h3>
+    <button @click="volume += 2">Increase</button>
+    <button @click="volume -= 2">Decrease</button>
 
-    <h2 v-for="item in expensiveItems" :key="item.id">
-      {{ item.title }}
-      -
-      {{ item.price }}
-    </h2>
   </div>
 </template>
 
@@ -24,45 +13,16 @@ export default {
   name: 'App',
   data() {
     return {
-      firstName: "Hafiz",
-      lastName: "Ahmad",
-      items: [
-        {
-          id: 1,
-          title: 'TV',
-          price: 200
-        },
-        {
-          id: 2,
-          title: 'Watch',
-          price: 150
-        },
-        {
-          id: 3,
-          title: 'Screen',
-          price: 170
-        },
-      ],
-      country: ''
+      volume: 0
     }
   },
-  methods: {
-    addItem() {
-      this.items.push({ id: 4, title: 'keyBoard', price: 50 });
-    },
-    getTotal() {
-      return this.items.reduce((total, curr) => total + curr.price, 0);
-    }
-  },
-  computed: {
-    FullName() {
-      return `${this.firstName} ${this.lastName}`;
-    },
-    total() {
-      return this.getTotal();
-    },
-    expensiveItems() {
-      return this.items.filter(item => item.price > 40);
+  methods: {},
+  computed: {},
+  watch: {
+    volume(newValue, oldValue) {
+      if (newValue > oldValue && newValue === 16) {
+        alert('the value is heigh');
+      }
     }
   }
 }
