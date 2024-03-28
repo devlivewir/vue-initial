95ca7cf3c9b1915028f9aecc4d35ed81068e679f
Ahmad Malik <85894787+ciahmad@users.noreply.github.com>
2024-03-28 12:26:10 +0500

    multiple inject component

--- src/components/ComponentC.vue
+++ src/components/ComponentC.vue
@@ -1,3 +1,19 @@
 <template>
-    
+    <div>
+        <h2>Component C</h2>
+        <ComponentE />
+    </div>
 </template>
+
+<script>
+import ComponentE from './components/ComponentE';
+export default {
+    name:'ComponentC',
+    components:{
+        ComponentE
+    }
+
+}
+</script>
+
+<style scoped></style>
--- src/components/ComponentE.vue
+++ src/components/ComponentE.vue
@@ -1,3 +1,18 @@
 <template>
-    
+    <div>
+        <h2>Component E</h2>
+        <ComponentF />
+    </div>
 </template>
+
+<script>
+import ComponentF from './ComponentF';
+export default {
+    name: 'ComponentF',
+    components: {
+        ComponentF
+    }
+}
+</script>
+
+<style scoped></style>
